97f9662a01325cb4b010229806b5e7e8540b5c0a
nawal-ahmad <nawal.abualshaikh@optimumpartners.co>
2022-11-02 18:18:11 +0300

    indentation

--- src/App.js
+++ src/App.js
@@ -7,21 +7,21 @@ import Crew from './pages/Crew';
 import Technology from './pages/Technology';
 import NotFound from './pages/NotFound';
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element=<Navigate to="/home" /> />
-          <Route path='/home' element={<Home />} />
-          <Route path='/destinations' element={<Destination />} />
-          <Route path='/crew' element={<Crew />} />
-          <Route path='/technology' element={<Technology />} />
-          <Route path='*' element={< NotFound />} />
-        </Routes>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element=<Navigate to="/home" /> />
+                    <Route path='/home' element={<Home />} />
+                    <Route path='/destinations' element={<Destination />} />
+                    <Route path='/crew' element={<Crew />} />
+                    <Route path='/technology' element={<Technology />} />
+                    <Route path='*' element={< NotFound />} />
+                </Routes>
+            </Router>
+        );
+    }
 }
 
 export default App;
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 import './NotFound.scss';
 
 export default class NotFound extends Component {
-  render() {
-    return (
-      <div className='not-found'>
-        <div className='content'>
-          <h1 className='heading'>Page Not Found</h1>
-          <p className='description'>Looks like you've followed a broken link or entered a URL that doesn't exist on this site.</p>
-          <Link className='home-link' to='/home'>Back to our site</Link>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='not-found'>
+                <div className='content'>
+                    <h1 className='heading'>Page Not Found</h1>
+                    <p className='description'>Looks like you've followed a broken link or entered a URL that doesn't exist on this site.</p>
+                    <Link className='home-link' to='/home'>Back to our site</Link>
+                </div>
+            </div>
+        );
+    }
 }
